0fa347488cb09ca52c1e748d4f08e8bfb252ab5a
ashahbazFV <59893522+ashahbazFV@users.noreply.github.com>
2020-01-24 15:23:59 -0500

    Delete jsonl_to_json.sh

--- jsonl_to_json.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "[" > "Large_Even.json"
-while IFS="\n" read -r Complaint
-do
-    echo "$Complaint," >> "Large_Even.json"
-done < "/home/ashahbaz/Downloads/Large_Predictions_Even.jsonl"
-sed -i '$ s/.$//' Large_Even.json
-echo "]" >> "Large_Even.json"
